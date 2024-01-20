6ef1de874d591fc5681882ed472f91a64df5b1a2
beyzaguney <49661382+beyzaguney@users.noreply.github.com>
2024-01-21 02:53:30 +0300

    Add project files.

---
+++ Model/About.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    //internal->erişim belirleyici
+    public class About : BaseModel
+    {
+        public string Description2 { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string Mail { get; set; }
+        public string Job { get; set; }
+    }
+}
---
+++ Model/BaseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class BaseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime DataCreated { get; set; } = DateTime.Now;
+        public DateTime? DataModified { get; set; }
+        public bool IsActive { get; set; } = true;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
